e85de79bcb164781c7e3d3751de8831728a4017d
TruthFire <truth.fire@yandex.ru>
2022-12-10 20:21:02 +0200

    db change

--- connect.js
+++ connect.js
@@ -1,9 +1,10 @@
 import mysql from 'mysql';
 
 export const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: 'db-mysql-fra1-34081-do-user-13073081-0.b.db.ondigitalocean.com',
+  user: 'doadmin',
   password: '',
   database: 'gamerbux',
+  port: '25060',
   multipleStatements: true,
 });
